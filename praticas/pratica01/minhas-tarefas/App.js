28018671cdfd63850ed5de2cea039838a0da4892
João Pedro Campos <jpgoncalves1406@gmail.com>
2026-03-13 10:51:09 -0300

    pratica 01

--- praticas/pratica01/minhas-tarefas/App.js
+++ praticas/pratica01/minhas-tarefas/App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import {titulo} from './util';
+import titulo_padrao from './util';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text> {titulo_padrao} </Text>
+      <Text> {titulo} </Text>
+      <Button title='Clique aqui' />
       <StatusBar style="auto" />
     </View>
   );
