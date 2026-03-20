8a9d3bef31b9e9d116607f7c44dae16e772326aa
João Pedro Campos <jpgoncalves1406@gmail.com>
2026-03-20 15:09:11 -0300

    Pratica 02

---
+++ praticas/pratica02/minhas-tarefas/components/MetaInput.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { View, TextInput, Button, StyleSheet } from 'react-native';
+
+function MetaInput(props) {
+  return (
+    <View style={styles.inputContainer}>
+      <View style={{ width: '65%' }}>
+        <TextInput
+          style={styles.inputText}
+          placeholder={props.placeholder}
+          onChangeText={props.onInputChange}
+          value={props.inputValue}
+        />
+      </View>
+
+      <View style={{ width: '30%' }}>
+        <Button 
+          title={props.buttonTitle} 
+          onPress={props.onAddMeta} 
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flex: 1,
+    marginBottom: 24, // Espaçamento opcional para separar da lista
+  },
+  inputText: {
+    borderColor: '#cccccc',
+    borderWidth: 1,
+    padding: 8,
+  },
+});
+
+export default MetaInput;
